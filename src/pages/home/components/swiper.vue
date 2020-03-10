a0708bbc028447b9b1da6af56b385f75e5322099
anglegigi <angelqiqiwang@gmail.com>
2020-03-10 10:06:12 +0800

    swiper-code

---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/201310/08/fde0fbd156f10f40c8d65eac.jpg_600x330_788edbfd.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 52%;
+  background: #eee;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
