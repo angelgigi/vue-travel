53c7542e1048a7fd82f47fbb112eba4f7c54328e
anglegigi <angelqiqiwang@gmail.com>
2020-03-13 15:37:14 +0800

    add alphabet touch

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -2,46 +2,54 @@
   <div class="list" ref="wrapper">
     <div>
       <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{item.name}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list">
+          <div
+            class="item border-bottom"
+            v-for="innerItem of item"
+            :key="innerItem.id"
+          >{{innerItem.name}}</div>
         </div>
       </div>
     </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div class="button-wrapper" v-for="item of hot" :key="item.id">
-          <div class="button">{{item.name}}</div>
-        </div>
-
-      </div>
-    </div>
-    <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
-      <div class="title border-topbottom">{{key}}</div>
-      <div class="item-list">
-        <div class="item border-bottom"
-             v-for="innerItem of item"
-             :key="innerItem.id"
-        >{{innerItem.name}}</div>
-      </div>
-      
-    </div>
-    </div>
-    
   </div>
 </template>
 
 <script>
-import Bscroll from 'better-scroll'
+import Bscroll from "better-scroll";
 export default {
   props: {
-    hot:Array,
-    cities:Object
+    hot: Array,
+    cities: Object,
+    letter:String
   },
-  mounted (){
-    this.scroll = new Bscroll(this.$refs.wrapper)
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper);
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        //scrollToElement(el, time, offsetX, offsetY, easing) 滚动到某个元素，el（必填）表示 dom 元素，
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 };
 </script>
